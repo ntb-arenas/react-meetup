38f967d4547da8fae57abfb883bb61b820df9cb6
Niño Arenas <ntb.arenas@protonmail.com>
2023-01-07 12:08:48 +0000

    Updated newMeetupForm to firebase

--- src/Pages/NewMeetup.jsx
+++ src/Pages/NewMeetup.jsx
@@ -1,11 +1,29 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const NewMeetupPage = () => {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch("https://react-meetup-7372e-default-rtdb.europe-west1.firebasedatabase.app/meetups.json", {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(meetupData),
+    }).then(() => {
+      history.replace("/");
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
--- src/components/meetups/NewMeetupForm.jsx
+++ src/components/meetups/NewMeetupForm.jsx
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 import Card from "../ui/Card";
 
-const NewMeetupForm = () => {
+const NewMeetupForm = ({ onAddMeetup }) => {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const meetupData = {
+      title: titleInputRef.current.value,
+      image: imageInputRef.current.value,
+      address: addressInputRef.current.value,
+      description: descInputRef.current.value,
+    };
+
+    onAddMeetup(meetupData);
+  }
+
   return (
     <Card>
-      <form className="p-1">
+      <form className="p-5" onSubmit={submitHandler}>
         <div className="mb-1">
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input className="ml-5 border border-black" type="text" required id="title" ref={titleInputRef} />
+        </div>
+        <div className="mb-1">
+          <label htmlFor="image">Meetup Image</label>
+          <input className="ml-5 border border-black" type="url" required id="image" ref={imageInputRef} />
         </div>
         <div className="mb-1">
           <label htmlFor="address">Meetup address</label>
-          <input type="url" required id="address" />
-        </div>
-        <div className="mb-1">
-          <label htmlFor="address">Meetup address</label>
-          <input type="text" required id="address" />
+          <input className="ml-5 border border-black" type="text" required id="address" ref={addressInputRef} />
         </div>
         <div className="mb-1">
           <label htmlFor="description">Meetup description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea className="border border-black" id="description" required rows="5" ref={descInputRef}></textarea>
         </div>
         <div className="mb-1">
-          <button>Add Meetup</button>
+          <button className="px-5 py-2 border ">Add Meetup</button>
         </div>
       </form>
     </Card>
